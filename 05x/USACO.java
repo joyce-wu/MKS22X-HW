f2442cf488a0e5164d3e2dc27e1cae1ca2d2a355
Joyce Wu <jwu26@stuy.edu>
2017-03-12 16:10:38 -0400

    silver finished

--- 05x/USACO.java
+++ 05x/USACO.java
@@ -84,12 +84,110 @@ public class USACO{
 	
 	
     public int silver(String filename){
+	try{
+	    File infile = new File(filename);
+	    Scanner inf = new Scanner(infile);
+	    String header = inf.nextLine();
+
+	    Scanner headerS = new Scanner(header);
+	    int N = headerS.nextInt();
+	    int M = headerS.nextInt();
+	    int T = headerS.nextInt();
+
+	    int[][] past1 = new int[N][M];
+	    int[][] past2 = new int[N][M];
+	    for(int r = 0; r < N; r++){
+		String row = inf.nextLine();
+		for(int c = 0; c < M; c++){
+		    String charac = row.substring(c, c+1);
+		    if(charac.equals("*")){
+			past1[r][c] = -1;
+			past2[r][c] = -1;
+		    }if(charac.equals(".")){
+			past1[r][c] = 0;
+			past2[r][c] = 0;
+		    }
+		}
+	    }
+
+	    String info = inf.nextLine();
+	    Scanner infoS = new Scanner(info);
+	    int R1 = infoS.nextInt()-1;
+	    int C1 = infoS.nextInt()-1;
+	    int R2 = infoS.nextInt()-1;
+	    int C2 = infoS.nextInt()-1;
+
+	    for(int time = 0; time <= T; time++){
+		if(time == 0){
+		    past2[R1][C1] = 1;
+		}else if(time%2 != 0){
+		    for(int r = 0; r < N; r++){
+			for(int c = 0; c < M; c++){
+			    if(past2[r][c] != 0 && past2[r][c] != -1){
+				past1[r][c] = 0;
+			    }if(past1[r][c] != -1){
+				past1[r][c] = findMoves(r, c, past2);
+			    }
+			}
+		    }
+		}else{
+		    for(int r = 0; r < N; r++){
+			for(int c = 0; c < M; c++){
+			    if(past1[r][c] != 0 && past2[r][c] != -1){
+				past2[r][c] = 0;
+			    }if(past2[r][c] != -1){
+				past2[r][c] = findMoves(r, c, past1);
+			    }
+			}
+		    }
+		}
+		toPrint(past2);
+		toPrint(past1);
+	    }
+	    if(T%2 == 0){
+		return past2[R2][C2];
+	    }else{
+		return past1[R2][C2];
+	    }
+		
+	}catch(FileNotFoundException e){
+	    System.out.println("File not found.");
+	    System.exit(0);
+	}
 	return -1;
     }
 
+    public void toPrint(int[][] past1){
+	//System.out.println(N + " " + M + " " + T);
+	for(int r = 0; r < past1.length; r++){
+	    for(int c = 0; c < past1[0].length; c++){
+		System.out.print(past1[r][c] + " ");
+	    }
+	    System.out.println("");
+	}
+	System.out.println("");
+    }
+	
+
+    public int findMoves(int r, int c, int[][] pasture){
+	int total = 0;
+	if(r > 0 && pasture[r-1][c] != -1){ 
+	    total += pasture[r-1][c];
+	}if(r < pasture.length-1 && pasture[r+1][c] != -1){
+	    total += pasture[r+1][c];
+	}if(c > 0 && pasture[r][c-1] != -1){
+	    total += pasture[r][c-1];
+	}if(c < pasture[0].length-1 && pasture[r][c+1] != -1){
+	    total += pasture[r][c+1];
+	}
+	return total;
+    }
+		    
+
     public static void main(String[] args){
 	USACO bloop = new USACO();
-	System.out.println(bloop.bronze("infile1.txt")); //342144
+	//System.out.println(bloop.bronze("infile1.txt")); //342144
+	System.out.println(bloop.silver("ctravel.in"));
     }
 
     
