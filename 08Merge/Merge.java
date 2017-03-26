5304a03708483aca9686eb4867b795a1bceacc30
Joyce Wu <jwu26@stuy.edu>
2017-03-26 18:50:16 -0400

    dont know whats wrong but mergesort doesn't work

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -1,9 +1,6 @@
 import java.util.*;
 
 public class Merge{
-    public static void mergesort(int[] ary){
-    }
-
     public static void merge(int[] a, int[] b, int[] destination){
 	int ai = 0;
 	int bi = 0;
@@ -28,18 +25,38 @@ public class Merge{
 	for(int i = index; i < destination.length; i++){
 	    destination[i] = a[aI];
 	    aI++;
+	    System.out.println(Arrays.toString(destination));
+	}
+    }
+
+    public static void mergesort(int[] ary){
+	if(ary.length == 1){
+	    return;
+	}else{
+	    int[] left = new int[ary.length/2];
+	    for(int i = 0; i < left.length; i++){
+		left[i] = ary[i];
+	    }
+	    int[] right = new int[ary.length/2];
+	    for(int i = 0, n = ary.length/2; i < right.length; i++, n++){
+		right[i] = ary[n];
+	    }
+	    mergesort(left);
+	    mergesort(right);
+	    merge(left, right, ary);
 	}
     }
 
     public static void main(String[] args){
 	int[] a = {-1, 2, 4, 6, 8, 10, 1000, 1000, 1000, 2011, 2013, 44444};
-	int[] b = {1, 3, 5, 6, 7, 9};
+	int[] b = {1, -1, 4, 3, 7, 2};
 	int[] d = new int[a.length + b.length];
 	int[] c = {9, 10, 14};
 	int[] e = {};
 	int[] hi = new int[3];
 	
-	merge(a, b, d);
-	System.out.println(Arrays.toString(d));
+	//merge(a, b, d);
+	mergesort(b);
+	System.out.println(Arrays.toString(b));
     }
 }
