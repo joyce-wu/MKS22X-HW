537913852184f7f3b039cec2d4ee3065e053931b
Joyce Wu <jwu26@stuy.edu>
2017-04-16 14:30:55 -0400

    all working methods

--- 09Lists/MyLinkedList.java
+++ 09Lists/MyLinkedList.java
@@ -1,12 +1,12 @@
 public class MyLinkedList{
-    LNode start;
-    LNode tail;
+    LNode start, tail;
     int size;
 
     public MyLinkedList(){
-	start = null;
-	tail = null;
-	size = 0;
+    }
+    
+    public int size(){
+	return size;
     }
 
     private LNode getNthNode(int n){
@@ -14,18 +14,21 @@ public class MyLinkedList{
 	    throw new IndexOutOfBoundsException();
 	}
 	LNode current = start;
-	for(int i = 0; i<size-1; i++){
+	for(int i = 0; i<size(); i++){
 	    if(n == i){
 		return current;
 	    }else{
 		current = current.next;
 	    }
 	}
+	return start;
     }
-
+    
     private void addAfter(LNode location, LNode toBeAdded){
 	if(location == tail){
-	    add(toBeAdded.value);
+	    tail.next = toBeAdded;
+	    toBeAdded.prev = tail;
+	    tail = toBeAdded;
 	}else{
 	    toBeAdded.next = location.next;
 	    toBeAdded.prev = location;
@@ -33,7 +36,7 @@ public class MyLinkedList{
 	    location.next = toBeAdded;
 	}
     }
-
+    
     public void remove(LNode target){
 	if(start == target){
 	    start = start.next;
@@ -42,36 +45,12 @@ public class MyLinkedList{
 	    tail = tail.prev;
 	    tail.next = null;
 	}else{
-	    target.prev.next = x.next;
+	    target.prev.next = target.next;
 	    target.next.prev = target.prev;
 	}
+	size--;
     }
     
-    public boolean add(int value){
-	add(size, value);
-	return true;
-    }
-
-    public int size(){
-	return size;
-    }
-
-    public int get(int index){
-	if(index < 0 || index >= size()){
-	    throw new IndexOutOfBoundsException();
-	}
-	return getNthNode(index).value;
-    }
-
-    public int set(int index, int newValue){
-	if(index < 0 || index >= size()){
-	    throw new IndexOutOfBoundsException();
-	}
-	int old = getNthNode(index).value;
-	getNthNode(index).value = newValue;
-	return old;
-    }
-
     public String toString(){
 	if(size == 0){
 	    return "[]";
@@ -89,7 +68,28 @@ public class MyLinkedList{
 	ans += current.value + "]";
      	return ans;
     }
+        
+    public boolean add(int value){
+	add(size, value);
+	return true;
+    }
+    
+    public int get(int index){
+	if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+	}
+	return getNthNode(index).value;
+    }
 
+    public int set(int index, int newValue){
+	if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+	}
+	int old = getNthNode(index).value;
+	getNthNode(index).value = newValue;
+	return old;
+    }
+    
     public int indexOf(int value){
 	LNode current = start;
 	for(int i = 0; i < size; i++){
@@ -103,48 +103,83 @@ public class MyLinkedList{
     }
 
     public void add(int index, int value){
+	if(index < 0 || index > size()){
+	    throw new IndexOutOfBoundsException();
+	}
 	LNode newL = new LNode(value);
 	if(index == 0){
-	    newL.next = start;
-	    start.prev = newL;
-	    start = newL;
-    	}else if(index == size()){
+	    if(size == 0){
+		start = newL;
+		tail = newL;
+	    }else{
+		newL.next = start;
+		start.prev = newL;
+		start = newL;
+	    }
+	}else if(index == size()){
+	    addAfter(tail, newL);
 	}else{
-	    LNode current = getNthNode(index);
-	    
+	    addAfter(getNthNode(index-1), newL);	    
 	}
+	size++;
     }
 
     public int remove(int index){
-	remove(getNode(index));
+	if(index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
+	}
+	int value = get(index);
+	remove(getNthNode(index));
+	return value;
     }
 
-    public class LNode{
+    public String toStringDebug(){
+	if(size == 0){
+	    return "[]";
+	}if(size == 1){
+	    return "[(null)" + start.value + "(null)]";
+	}
+	String ans = "[";
+	LNode current = start;
+	ans += "(null)" + start.value + "(" + start.next.value + "), ";
+	for(int i = 0; i < size-2; i++){
+	    current = current.next;
+	    ans += "(" + current.prev.value + ")" + current.value + "(" + current.next.value + "), ";
+	}
+	current = current.next;
+	ans += "(" + current.prev.value + ")" + current.value + "(null)]";
+	return ans;
+    }
+
+    private class LNode{
 	int value;
-	LNode next;
-	LNode prev;
+	LNode next,prev;
 	
 	public LNode(int val){
 	    value = val;
-	    next = null;
-	    prev = null;
 	}
-	public LNode(int val, LNode node){
-	    value = val;
-	    next = node;
-	    prev = null;
+	public String toString(){
+	    return value+" ";
 	}
     }
 
     public static void main(String[] args){
 	MyLinkedList hi = new MyLinkedList();
+	System.out.println(hi.toStringDebug());
 	hi.add(7);
 	hi.add(10);
-	hi.add(11);
-	for(int i = 0; i<15; i++){
-	    hi.add(i);
-       }
-       System.out.println(hi.indexOf(10));
-       System.out.println(hi.toString());
-   }
+	hi.add(-2);
+	hi.add(1000000);
+	hi.add(222);
+	hi.add(4);
+	hi.add(20001);
+	hi.add(20001);
+	System.out.println(hi.toString());
+	hi.add(0, 44);
+	System.out.println(hi.toString());
+	hi.add(8, 10000000);
+	System.out.println(hi.toString());
+	hi.add(10, -22);
+	System.out.println(hi.toString());
+    }
 }
